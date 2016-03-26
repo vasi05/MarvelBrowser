e91b2231cb572149ba4e746909dd8b829c4d6b3a
vasi05 <croitoruvs@gmail.com>
2016-03-26 11:06:10 +0200

    Initial Commit

---
+++ MarvelBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MarvelBrowser
+//
+//  Created by Vasile Croitoru on 26/03/16.
+//  Copyright © 2016 Vasile Croitoru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
