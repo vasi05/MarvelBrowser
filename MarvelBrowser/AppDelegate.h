c247f78f6e4895afa127e25df3a3bab7ed2204b5
vasi05 <croitoruvs@gmail.com>
2016-03-26 18:12:17 +0200

    del

--- MarvelBrowser/AppDelegate.h
+++ MarvelBrowser/AppDelegate.h
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.h
-//  MarvelBrowser
-//
-//  Created by Vasile Croitoru on 26/03/16.
-//  Copyright © 2016 Vasile Croitoru. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
