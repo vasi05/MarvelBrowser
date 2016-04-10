5ef939542b136a30194ceb3b1250e3083e71dbab
vasi05 <croitoruvs@gmail.com>
2016-04-10 20:01:55 +0300

    added calltomarvel acceptance test

--- AcceptanceTests/AcceptanceTests.m
+++ AcceptanceTests/AcceptanceTests.m
@@ -1,4 +1,10 @@
-
+//
+//  AcceptanceTests.m
+//  AcceptanceTests
+//
+//  Created by Vasile Croitoru on 03/04/16.
+//  Copyright © 2016 Vasile Croitoru. All rights reserved.
+//
 
 #import <XCTest/XCTest.h>
 #import "VSMarvelAuthentification.h"
@@ -9,32 +15,40 @@
 
 @implementation AcceptanceTests
 
--(void)testCallToMarvel_ShouldGetStatuss200OK
+-(void)testCallToMarvel_ShouldGetHTTPStatusCode200
 {
     XCTestExpectation *expectation = [self expectationWithDescription:@"Testing CallToMarvel_ShouldGetStatus"];
     
     VSMarvelAuthentification * sut = [[VSMarvelAuthentification alloc] init];
-    NSString *URLString = [@"https://gateway.marvel.com/v1/public/characters?nameStartsWith=Spider"
-                           stringByAppendingString:[sut urlParameters]];
-    NSURL *url = [NSURL URLWithString:URLString];
+    NSString *validQueryMissingAuthentification = @"https://gateway.marvel.com/v1/public/characters?nameStartsWith=Spider";
+    NSURL *validQueryURL = [NSURL URLWithString:[validQueryMissingAuthentification stringByAppendingString:[sut urlParameters]]];
+    
+    __block NSHTTPURLResponse * httpResponse;
+    
+    [self startGetRequestToURL:validQueryURL withCompletionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        httpResponse = (NSHTTPURLResponse *)response;
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:5.0f handler:^(NSError * _Nullable error) {
+        XCTAssertEqualObjects(@200, [NSNumber numberWithInteger:httpResponse.statusCode]);
+    }];
+    
+}
+
+
+-(void)startGetRequestToURL:(NSURL *)url withCompletionHandler:(void(^)(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error))completionHandler
+{
     NSURLSessionConfiguration * sessionConnfig = [NSURLSessionConfiguration defaultSessionConfiguration];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConnfig
                                                           delegate:nil
                                                      delegateQueue:nil];
-    
-    __block NSHTTPURLResponse * httpResponse;
-    
+
     NSURLSessionDataTask *dataTask = [session dataTaskWithURL:url
-                                            completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-                                                    httpResponse = (NSHTTPURLResponse *)response;
-                                                   [expectation fulfill];
-                                            }];
+                                            completionHandler:completionHandler];
     
     [dataTask resume];
-    
-    [self waitForExpectationsWithTimeout:5000.0f handler:^(NSError * _Nullable error) {
-        XCTAssertEqualObjects(@200, [NSNumber numberWithInteger:httpResponse.statusCode]);
-    }];
+
     
 }
 
